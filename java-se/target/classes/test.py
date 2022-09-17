ede48dd4a2661b472ef0f4da5ae5adb324189490
superkuang1997 <1120053798@qq.com>
2022-09-17 16:49:27 +0800

    update .gitignore

--- java-se/target/classes/test.py
+++
@@ -1,7 +0,0 @@
-def add(x, y):
-    return x + y
-
-
-res = add(5, 10)
-print(res)
-print("hello world")
--- java-se/target/classes/test.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-path=`pwd`
-touch plsfindme
-echo hahaha > plsfindme
-file_path=$path/test.py
-res=`python $file_path $@`
-echo $res
-echo 1122222
-echo 11111111
